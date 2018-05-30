8d95b9a8fb419ecd3cda1a4f2190959493c07d08
nul <jackaklassen.jak@gmail.com>
2018-05-30 16:09:35 -0700

    Add files via upload

---
+++ DiceRoll.py
@@ -0,0 +1,17 @@
+#need random
+import random
+#random.randint(a, b)
+
+#Return a random integer N such that a <= N <= b.
+
+#Docs: https://docs.python.org/3.1/library/random.html#random.randint
+
+
+#promt the user to roll wiht infinite while loop
+while(usrInput exit)
+
+print("press enter to roll the dice")
+#roll dice
+#call random 1 to 6
+print(random.randint(0,9))
+#print output
