8eb6eebc9f67c0fa7c3d02b67bee08a355b73d08
Jack <jackaklassen.jak@gmail.com>
2019-05-11 15:32:46 -0700

    Update DiceRoll.py

--- DiceRoll.py
+++ DiceRoll.py
@@ -10,9 +10,8 @@
 
 
 import random
-#random docs: https://docs.python.org/3.1/library/random.html#random.randint
 
-#promt the user to roll wiht infinite while loop
+#promt the user to roll with an infinite while loop
 
 def RandRoll(NumOfSides):
     DiceFace =  random.randint(0,NumOfSides)
